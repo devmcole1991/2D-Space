dd19c96f939fbf8d0712dc8529cd75ee7e57ee22
devmcole1991 <devmcole1991@knights.ucf.edu>
2018-02-25 15:54:18 -0500

    Pickups

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D info)
+    {
+		if (info.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+	}
+}
