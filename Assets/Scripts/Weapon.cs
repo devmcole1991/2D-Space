24b9940155b926e3227f358b9fd0ca35cdf10374
devmcole1991 <devmcole1991@knights.ucf.edu>
2018-03-04 13:28:52 -0500

    Turret Prefab and Animations

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,7 +6,7 @@ namespace Assets.GameLogic.Core
     public class Weapon : MonoBehaviour, IUpdatable
     {
         private IPlatformCharacterController controller;
-        new private SpriteRenderer renderer;
+        private SpriteRenderer renderer;
 
         [SerializeField] private float fireRate;
         [SerializeField] private float timeToFire;
@@ -16,11 +16,17 @@ namespace Assets.GameLogic.Core
         public Transform MuzzleFlashPrefab;
         Transform firePoint;
 
+        Vector2 posFirePoint;
+        Vector2 negFirePoint;
+
         private void Awake()
         {
             controller = GetComponent<IPlatformCharacterController>();
             renderer = GetComponent<SpriteRenderer>();
             firePoint = transform.Find("FirePoint");
+
+            posFirePoint = new Vector2(firePoint.localPosition.x, firePoint.localPosition.y);
+            negFirePoint = new Vector2(-firePoint.localPosition.x, firePoint.localPosition.y);
 
             if (firePoint == null)
             {
@@ -42,35 +48,36 @@ namespace Assets.GameLogic.Core
         {
             if (renderer.flipX)
             {
-                firePoint.localPosition = new Vector3(-21, 21, 0);
+                firePoint.localPosition = new Vector3(negFirePoint.x, negFirePoint.y, 0);
                 firePoint.localRotation = new Quaternion(0, 180, 0, 0);
             }
             else
             {
-                firePoint.localPosition = new Vector3(21, 21, 0);
+                firePoint.localPosition = new Vector3(posFirePoint.x, posFirePoint.y, 0);
                 firePoint.localRotation = new Quaternion(0, 0, 0, 0);
             }
 
+            /*
             if (fireRate == 0)
             {
-                //if (controller.ShootPressed == 1)
-                //    Shoot();
+                if (controller.ShootPressed == 1)
+                    Shoot();
             }
             else
             {
-                //if (controller.ShootHeld == 1 && Time.time > timeToFire)
-                //{
-                //    timeToFire = Time.time + 1 / fireRate;
-                //    Shoot();
-                //}
-            }
+                if (controller.ShootHeld == 1 && Time.time > timeToFire)
+                {
+                    timeToFire = Time.time + 1 / fireRate;
+                    Shoot();
+                }
+            }*/
         }
 
         void Shoot()
         {
-            Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
             Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
-            RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+            RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
             Effect();
         }
 
